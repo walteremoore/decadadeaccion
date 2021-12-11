1af36c93c107cd221bf70f2d2f0c569f6effd202
walteremoore <walterenriquemoore@gmail.com>
2021-12-11 11:58:28 -0500

    clase formulario

--- apps/articulos/forms.py
+++ apps/articulos/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Articulo
 
 class ArticuloForm(forms.ModelForm):
+    titulo = forms.CharField(label="Título del artículo", widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Por favor ingrese el título del artículo"}))
     class Meta:
         model = Articulo
         fields = ["titulo", "contenido"]
