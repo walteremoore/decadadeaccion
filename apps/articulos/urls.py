4cc9ed27b1cb7202807ec63addbaa89dc21e21da
walteremoore <walterenriquemoore@gmail.com>
2021-12-11 11:36:34 -0500

    crear articulos

--- apps/articulos/urls.py
+++ apps/articulos/urls.py
@@ -5,5 +5,8 @@ app_name = "articulos"
 
 urlpatterns = [
     path('detalle/', views.detalle, name="detalle"),
+
+    #Admin
     path('admin/listar/', views.ListarAdmin.as_view(), name="admin_listar"),
+    path('admin/nuevo/', views.NuevoAdmin.as_view(), name="admin_nuevo"),
 ]
--- apps/articulos/views.py
+++ apps/articulos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from apps.articulos.models import Articulo
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from .forms import ArticuloForm
+from django.urls import reverse_lazy
 
 def detalle(request):
     contex = {}
@@ -10,3 +12,15 @@ class ListarAdmin(ListView):
     template_name="articulos/admin/listar.html"
     model = Articulo
     context_object_name="articulos"
+
+    # def get_queryset(self):
+    #     self.request
+    #     return Articulo.objects.filter(id=1)
+    
+class NuevoAdmin(CreateView):
+    template_name = "articulos/admin/nuevo.html"
+    model = Articulo
+    form_class = ArticuloForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy("articulos:admin_listar")
