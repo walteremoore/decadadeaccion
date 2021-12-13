55284401d1cbca97942676c191ad10902c92e812
walteremoore <walterenriquemoore@gmail.com>
2021-12-12 22:03:24 -0500

    arreglar imagenes

--- decadadeaccion/settings.py
+++ decadadeaccion/settings.py
@@ -137,10 +137,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static",
     )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- decadadeaccion/urls.py
+++ decadadeaccion/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from . import views 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
@@ -13,4 +15,4 @@ urlpatterns = [
     path("logout/", auth_views.logout_then_login, name="logout"),
     #Includes
     path('articulos/', include('apps.articulos.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
