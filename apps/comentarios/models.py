df9923b39d7f2923e42ee103c85626c842ef491d
walteremoore <walterenriquemoore@gmail.com>
2021-12-16 13:00:28 -0500

    comentarios

--- apps/comentarios/models.py
+++ apps/comentarios/models.py
@@ -3,13 +3,13 @@ from apps.articulos.models import Articulo
 from apps.usuarios.models import Usuario
 
 class Comentario(models.Model):
-    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE, null=True)
+    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
     contenido = models.TextField(blank=True, null=True)
-    estado = models.BooleanField(blank=True, null=True) # True=aprobado, False=baja_logica
+    estado = models.BooleanField(blank=True, null=True, default=True) # True=aprobado, False=baja_logica
     visibilidad = models.BooleanField(blank=True, null=True, default=False) # True=visible, False=pendiente de moderar
-    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True)
-    fecha_creacion = models.DateTimeField(auto_now_add=True, blank=True)
-    titulo = models.CharField(max_length=255, blank=True, null=True)
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    titulo = models.CharField(max_length=255)
     
     class Meta:
 	    db_table="comentarios"
